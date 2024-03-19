0c9ab78164a4989d17fc96ad1c446995213e3db4
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-03-19 14:19:34 +0545

    file reading handling by read readline readlines in python

--- file_read_handling.py
+++ file_read_handling.py
@@ -1,2 +1,10 @@
 with open('sample.txt', 'r') as file:
-    print(file.read())
+    # print(file.read())
+    # print(file.read(5))
+    # print(file.readlines())
+    # data = file.readlines()
+
+    # for x in data:
+    #     print(x)
+    for x in file:
+        print(x)
