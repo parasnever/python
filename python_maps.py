b4365547b53938809ec957ecb13b7150949948d1
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-04-12 13:12:30 +0545

    python maps

---
+++ python_maps.py
@@ -0,0 +1,11 @@
+menu = ['espresso', 'mocha', 'cappacino', 'latte', 'cortardo', 'americano']
+
+def find_coffee(coffee):
+    if coffee[0] == 'c':
+        return coffee
+
+map_coffee = map(find_coffee, menu)
+print(map_coffee)
+
+for x in map_coffee:
+    print(x)
