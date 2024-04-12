ac48d252e75c0bc743c0b9065e1f3e945ffc3b8a
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-04-12 13:15:40 +0545

    filter in python

--- python_maps.py
+++ python_maps.py
@@ -4,8 +4,14 @@ def find_coffee(coffee):
     if coffee[0] == 'c':
         return coffee
 
-map_coffee = map(find_coffee, menu)
-print(map_coffee)
+# map_coffee = map(find_coffee, menu)
+# print(map_coffee)
 
-for x in map_coffee:
+# for x in map_coffee:
+#     print(x)
+
+filter_coffee = filter(find_coffee, menu)
+print(filter_coffee)
+
+for x in filter_coffee:
     print(x)
