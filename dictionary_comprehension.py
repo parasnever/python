ce5670200b1bac585128df12a36cc67903273801
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-04-12 14:12:20 +0545

    dictionary comprehension example

---
+++ dictionary_comprehension.py
@@ -0,0 +1,15 @@
+# Using range() function and no input list
+usingrange = {x:x*2 for x in range(12)}
+print("Using range(): ", usingrange)
+
+# Lists
+months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
+number = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+# Using one input list
+numdict = {x:x**2 for x in number}
+print("Using one input list to create dict: ", numdict)
+
+# Using two input lists
+months_dict = {key:value for (key, value) in zip(number,months)}
+print("Using two lists: ", months_dict)
