6341c7892f68d91f696a115c39dc3b8e4a9006ba
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-04-12 10:47:16 +0545

    string reverse rescursion

---
+++ string_reverse_recursion.py
@@ -0,0 +1,9 @@
+def string_reverse(str):
+    if len(str) == 0:
+        return str
+    else:
+        return string_reverse(str[1:]) + str[0]
+
+str = "I love you"
+reverse = string_reverse(str)
+print(reverse)
